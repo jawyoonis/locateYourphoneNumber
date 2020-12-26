db58ab7e2e10c497506be7e987b0be6f24810bbb
Jamal Aw Yonis <awyoonisj@gmail.com>
2020-12-26 13:10:25 -0500

    second commit

--- locatePhoneNumbers.py
+++ locatePhoneNumbers.py
@@ -4,11 +4,12 @@ from phonenumbers import geocoder
 #from .carrierdata import CARRIER_DATA, CARRIER_LONGEST_PREFI
 
 
-phone_number1= phonenumbers.parse('+14845029957')
+phone_number1= phonenumbers.parse('+put your phone number here')
 print(phone_number1)
 print(geocoder.description_for_number(phone_number1, "en"))
 print(carrier.name_for_number(phone_number1, "en"))
-phone_number2= phonenumbers.parse('+252634118328')
+phone_number2= phonenumbers.parse('+put your phone number here')
 print(phone_number2)
 print(geocoder.description_for_number(phone_number2, "en"))
 print(carrier.name_for_number(phone_number2, "en"))
+n"))
